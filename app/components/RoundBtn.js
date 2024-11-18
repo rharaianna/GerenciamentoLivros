ccb2c699e97d411fd1f12927706967e97e01b54f
Rhara Ianna <rharaianna@gmail.com>
2024-11-18 08:51:03 -0300

    botao + configs iniciais

---
+++ app/components/RoundBtn.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {View, StyleSheet} from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+import colors from '../misc/colors'
+
+const RoundBtn = ({antIconName, size, color, style, onPress}) => {
+    return(
+        <AntDesign name={antIconName} size={size|| 24} color={color|| colors.LIGHT} style={[styles.icon, {...style}]} onPress={onPress}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    icon: {
+        backgroundColor: colors.PRIMARY,
+        padding:15,
+        borderRadius: 50,
+        elevation: 5,
+    },
+})
+
+export default RoundBtn;
---
+++ app/screens/NoteScreen.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+
+const NoteScreen = () => {
+    return(
+        <>
+        <StatusBar/>
+        <View style={StyleSheet.container}> 
+
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+})
+
+export default NoteScreen;
